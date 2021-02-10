70b1457a876d18a9e113e76ea288935ab1375c29
Mazev <77510376+Mazev@users.noreply.github.com>
2021-02-10 15:17:56 +0200

    python

--- 04.Exercise Lists as Stacks and Queues/05. Truck Tour.py
+++ 04.Exercise Lists as Stacks and Queues/05. Truck Tour.py
@@ -2,27 +2,21 @@ from collections import deque
 
 n = int(input())
 
-station = deque()
+station = deque([])
 
 for _ in range(n):
-    station.append(input())
+    station.append([el for el in input().split()])
 
-petrol_tank = 0
 
 for big_circle in range(n):
     is_valid = True
+    petrol_tank = 0
     for small_circle in range(n):
-        current_station = station.popleft()
-        petrol, distance = current_station.split()
-        petrol = int(petrol)
-        distance = int(distance)
-        petrol_tank += petrol
+        petrol_tank += int(station[small_circle][0]) - int(station[small_circle][1])
 
-        if petrol_tank >= distance:
-            petrol_tank -= distance
-            station.append(current_station)
-        else:
+        if petrol_tank < 0:
             is_valid = False
+            station.append(station.popleft())
             break
     if is_valid:
         print(big_circle)
