78ebcba67aa28f51533597fe07e06d54ce6dd3b0
Mazev <77510376+Mazev@users.noreply.github.com>
2021-02-11 19:19:07 +0200

    python

--- 06.Exercise Tuples and Sets/02. Sets of Elements.py
+++ 06.Exercise Tuples and Sets/02. Sets of Elements.py
@@ -1,4 +1,14 @@
-set_of_elemnts = input().split()
+def input_to_list(count):
+    lines = []
+    for _ in range(count):
+        lines.append(input())
+    return lines
 
 
-a = 5
+number = list(map(int, input().split()))
+set_1 = input_to_list(number[0])
+set_2 = input_to_list(number[1])
+
+unieque_elements = set(set_1).intersection(set_2)
+for i in unieque_elements:
+    print(i)
