0352430af28f056de5f6f710be8793818de7e955
Mazev <77510376+Mazev@users.noreply.github.com>
2021-02-13 22:19:48 +0200

    python

--- 08.Exercise Multidimensional Lists/02. 2x2 Squares in Matrix.py
+++ 08.Exercise Multidimensional Lists/02. 2x2 Squares in Matrix.py
@@ -1,11 +1,20 @@
-def reed_matrix():
+def reed_matrix(matrix):
     (rows_count, columns_count) = map(int, input().split())
     matrix = []
     for row_index in range(rows_count):
-        row = [x for x in input(). split()]
+        row = [x for x in input().split()]
         matrix.append(row)
     return matrix
 
 
-matrix = reed_matrix()
+def chek_elements_equal(row, col, matr):
+    curent_element = matr[row][col]
+    next_element_same_row = matr[row][col]
+    element_botom = matr[row][col]
+    element_botom_right = matr[row + 1][col + 1]
+    if curent_element == next_element_same_row and element_botom == element_botom_right:
+        return True
+    return False
 
+
+matrix = reed_matrix()
