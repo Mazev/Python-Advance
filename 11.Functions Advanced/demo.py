db0638a94311d1b15082484c612091fb2af41cfd
Mazev <77510376+Mazev@users.noreply.github.com>
2021-03-26 17:50:42 +0200

    python

--- 11.Functions Advanced/demo.py
+++ 11.Functions Advanced/demo.py
@@ -1,9 +1,34 @@
-def convert_iterable_to_absolute(num_list):
-    result = [abs(el) for el in num_list]
-    return result
+# def convert_iterable_to_absolute(num_list):
+#     result = [abs(el) for el in num_list]
+#     return result
+#
+#
+# num = [float(el) for el in input().split()]
+#
+# print(convert_iterable_to_absolute(num))
+
+# def greet_me(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f'{value}, {key}')
+#
+#
+# greet_me(Peter='Hello', Ivan='Bye')
 
 
-num = [float(el) for el in input().split()]
+# def some_func(arg1, *rest_arg):
+#     print(arg1 + sum(rest_arg))
+#
+#
+# some_func(5 , 5 , 10)
 
-print(convert_iterable_to_absolute(num))
 
+# staks
+
+# def say_hello_n_time(times):
+#     if times == 0:
+#         return
+#     print('Hello')
+#     say_hello_n_time(times - 1)
+#
+#
+# say_hello_n_time(5)
